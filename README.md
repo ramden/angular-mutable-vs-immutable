24f6d520157f4796f711082ad73b21a133d7f339
ramden <ramden@gmail.com>
2019-04-07 10:40:59 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-k2qzmk
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-k2qzmk)
---
+++ src/app/data-service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
+
+interface ICustomObject {
+  title: string,
+  date: Date
+}
+
+var listData: Array<ICustomObject> = [
+  {
+    title: "Item 1",
+    date: new Date()
+  }
+];
+
+@Injectable({
+  providedIn: 'root',
+})
+export class DataService {
+  private _listDataSubject = new BehaviorSubject(listData)
+  listDataObserver$ = this._listDataSubject.asObserver()
+
+  addItem(item: ICustomObject) {
+    listData.push(item);
+    this._listDataSubject.next(listData);
+  }
+
+  deleteItem(item: ICustomObject) {
+    listData = listData.filter(obj => obj !== item);
+    this._listDataSubject.next(listData);
+  }
+
+  updateItem(item: ICustomObject) {
+    let index = listData.indexOf(item);
+    listData[index] = item;
+  }
+}
